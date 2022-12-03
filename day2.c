d66ed4c6bd4ec4343724a542bf0b6cc2808c97d2
karena <karena@costarastrology.com>
2022-12-03 13:20:29 -0800

    Cleaned up Day 2 code

--- day2.c
+++ day2.c
@@ -6,50 +6,70 @@ int get_score(char other_player_move, char your_move);
 int main() {
 
     FILE *fp;
+
+    // Use this constant to normalize between A,B,C and X,Y,Z chars
+    const int NORMALIZE_TO_XYZ = 'X' - 'A'; // 23
+
+    // Set the line length to read from the file
     const int MAX_LINE_LENGTH = 5;
+
+    // The var to read in lines from the file
     char line[MAX_LINE_LENGTH];
 
-    // Assuming nums in file don't contain negative ints
-    int your_overall_score = 0; 
-    fp = fopen("suggested_moves.txt", "r");
+    // This will be the final score
+    int your_overall_score = 0;
 
+    // Open file and ensure it's valid
+    fp = fopen("suggested_moves.txt", "r");
     if(fp == NULL) {
         printf("invalid file");
         exit(1);
     }
 
+    // While we aren't at the end of the file, continue to read lines
+    // TODO: Don't assume input is formatted well
     while(fgets(line, sizeof(line), fp)!= NULL) {
-		char other_player_move = '\0';
-	    char your_move = '\0';
-	    sscanf(line, "%c %c\n", &other_player_move, &your_move);
+        char other_player_move = '\0';
+        char your_move = '\0';
 
-		// Normalize everything 
-		your_overall_score += get_score(other_player_move+23, your_move);
+        sscanf(line, "%c %c\n", &other_player_move, &your_move);
+
+        your_overall_score += get_score((other_player_move + NORMALIZE_TO_XYZ),
+            your_move);
     }
-	
-    printf("\nOverall Score: %d\n", your_overall_score);
+
+    printf("Overall Score: %d\n", your_overall_score);
 }
 
 int get_score(char other_player_move, char your_move) {
-	const int WIN_POINTS = 6;
-	const int TIE_POINTS = 3;
-	const int LOSE_POINTS = 0;
-	const int SHAPE_POINTS[] = {1, 2, 3};
-	const int YOU_LOSE[3][2] = {{'Y','X'},{'Z','Y'}, {'X','Z'}};
-	const int YOU_WIN[3][2] = {{'Z','X'},{'X','Y'},{'Y','Z'}};
-	
-	int your_score = 0;
-	if(your_move == other_player_move) {
-	    your_score = TIE_POINTS + SHAPE_POINTS[(your_move - 'X')];
-		printf("tie: %d %d\n", TIE_POINTS, SHAPE_POINTS[your_move -'X']);
-	} else if(*YOU_WIN[your_move - 'X'] == other_player_move) {
-		printf("win: %d %d\n", WIN_POINTS, SHAPE_POINTS[(your_move - 'X')]);
-	    your_score = WIN_POINTS + SHAPE_POINTS[(your_move - 'X')];
-		printf("YOUR SCORE: %d\n", your_score);
-	} else {
-		printf("lose: %d\n", SHAPE_POINTS[your_move - 'X']);
-		your_score = LOSE_POINTS + SHAPE_POINTS[(your_move - 'X')];	
+    // the number of points for your win/lose status
+    const int WIN_POINTS = 6;
+    const int TIE_POINTS = 3;
+    const int LOSE_POINTS = 0;
+
+    // the number of points for each shape (Rock, Paper, Scissors)
+    const int SHAPE_POINTS[] = {1, 2, 3};
+
+    // The set of pairs that result in you winning, with your move as the first element in each pair
+    const int YOU_WIN[3][2] = {{'Z','X'},{'X','Y'},{'Y','Z'}};
+
+    int your_score = 0;
+
+    // Change char into array index for the Shape array
+    int your_move_arr_index = your_move - 'X';
+
+    // Game is a tie
+    if(your_move == other_player_move) {
+        your_score = TIE_POINTS;
+    } else if(YOU_WIN[your_move_arr_index][0] == other_player_move) {
+        // You win
+        your_score = WIN_POINTS;
+    } else {
+        // You lose
+        your_score = LOSE_POINTS;
     }
-	printf("%d\n", your_score);
-    return your_score;	
+
+    // Your total score equals the win/lose/tie + number of points for R/P/S
+    your_score += SHAPE_POINTS[your_move_arr_index];
+    return your_score;
 }
--- day2_b.c
+++ day2_b.c
@@ -6,55 +6,77 @@ int get_score(char other_player_move, char your_move);
 int main() {
 
     FILE *fp;
+
+    // Set the line length to read from the file
     const int MAX_LINE_LENGTH = 5;
+
+    // The var to read in lines from the file
     char line[MAX_LINE_LENGTH];
 
-    // Assuming nums in file don't contain negative ints
-    int your_overall_score = 0; 
-    fp = fopen("suggested_moves.txt", "r");
+    // This will be the final score
+    int your_overall_score = 0;
 
+    // Open file and ensure it's valid
+    fp = fopen("suggested_moves.txt", "r");
     if(fp == NULL) {
         printf("invalid file");
         exit(1);
     }
 
+    // While we aren't at the end of the file, continue to read lines
+    // TODO: Don't assume input is formatted well
     while(fgets(line, sizeof(line), fp)!= NULL) {
-		char other_player_move = '\0';
-	    char result = '\0';
-	    sscanf(line, "%c %c\n", &other_player_move, &result);
+        char other_player_move = '\0';
+        char result = '\0';
 
-		// Normalize everything 
-		your_overall_score += get_score(other_player_move, result);
+        sscanf(line, "%c %c\n", &other_player_move, &result);
+
+        // Normalize everything 
+        your_overall_score += get_score(other_player_move, result);
     }
-	
-    printf("\nOverall Score: %d\n", your_overall_score);
+ 
+    printf("Overall Score: %d\n", your_overall_score);:re
 }
 
-int get_score(char other_player_move, char result) {
-	const int WIN_POINTS = 6;
-	const int TIE_POINTS = 3;
-	const int LOSE_POINTS = 0;
-	const int SHAPE_POINTS[] = {1, 2, 3};
-	const int YOU_WIN[3][2] = {{'B','A'},{'C','B'}, {'A','C'}};
-	const int YOU_LOSE[3][2] = {{'C','A'},{'A','B'},{'B','C'}};
+int get_score(char other_player_move, char result)
+{
+    // the number of points for your win/lose status
+    const int WIN_POINTS = 6;
+    const int TIE_POINTS = 3;
+    const int LOSE_POINTS = 0;
 
-	int your_score = 0;
-	int your_move = 0;
-	// You tie
-	if(result == 'Y') {
-	    your_score = TIE_POINTS + SHAPE_POINTS[other_player_move - 'A'];
-		printf("tie: %d %d\n", TIE_POINTS, SHAPE_POINTS[other_player_move -'A']);
-	} else if(result == 'Z') {
-		your_move = *YOU_WIN[other_player_move - 'A'];
-		printf("your move: %c\n", your_move);
-		printf("win: %d %d\n", WIN_POINTS, SHAPE_POINTS[(your_move - 'A')]);
-	    your_score = WIN_POINTS + SHAPE_POINTS[your_move - 'A'];
-	} else {
-		your_move = *YOU_LOSE[other_player_move - 'A'];
-		printf("your move: %c\n", your_move);
-		printf("lose: %d\n", SHAPE_POINTS[your_move - 'A']);
-		your_score = LOSE_POINTS + SHAPE_POINTS[your_move - 'A'];	
+    // the number of points for each shape (Rock, Paper, Scissors)
+    const int SHAPE_POINTS[] = {1, 2, 3};
+
+    // The set of pairs that result in you winning and losing, with the
+    // other player's move as the first element in each pair
+    const int YOU_WIN[3][2] = {{'B','A'},{'C','B'}, {'A','C'}};
+    const int YOU_LOSE[3][2] = {{'C','A'},{'A','B'},{'B','C'}};
+
+    int your_score = 0;
+
+    // your move as an array index into the shape array (R/P/S)
+    int your_move = 0;
+
+    // the other player's move, as an array index into the win/lose arrays
+    int other_player_move_index = other_player_move - 'A';
+
+    if(result == 'Y') {
+        // You tie; you have the same score
+        your_move = other_player_move;
+        your_score = TIE_POINTS;
+    } else if(result == 'Z') {
+        // You win
+        your_move = *YOU_WIN[other_player_move - 'A']; // R/P/S
+        your_score = WIN_POINTS;
+    } else {
+        // you lose
+        your_move = *YOU_LOSE[other_player_move - 'A']; // R/P/S
+        your_score = LOSE_POINTS;
     }
-	printf("%d\n", your_score);
-    return your_score;	
+
+    // Your score equals your win/lose/tie + the points for your shape
+    your_score += SHAPE_POINTS[your_move - 'A'];
+    printf("%d\n", your_score);
+    return your_score;
 }
