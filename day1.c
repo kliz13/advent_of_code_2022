6ac53aa34d5b18ff4df59fbce2ceb454f384162f
karena <karena@costarastrology.com>
2022-12-02 21:50:51 -0800

    day 1 of 2022 advent of code

---
+++ day1.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+    FILE *fp;
+    const int MAX_LINE_LENGTH = 10;
+    char line[MAX_LINE_LENGTH];
+
+    // Assuming nums in file don't contain negative ints
+    int max_calories_per_elf = 0; 
+    fp = fopen("calories.txt", "r");
+
+    if(fp == NULL) {
+        printf("invalid file");
+        exit(1);
+    }
+
+    int total_for_this_elf = 0;
+    
+    // We start out counting cals for a new elf (the first elf)
+    int new_elf = 1;
+	
+    while(fgets(line, sizeof(line), fp)!= NULL) {
+        if(*line == '\n') {
+	    if(total_for_this_elf > max_calories_per_elf) {
+                max_calories_per_elf = total_for_this_elf;
+	    }
+	    total_for_this_elf = 0;
+	    printf("%s\n", "next elf!");
+	    new_elf = 1;
+	} else {
+	    new_elf = 0;
+	    total_for_this_elf += atoi(line);
+            printf("%d\n", atoi(line));
+	}
+    }
+	
+
+    printf("\n\n%d\n", max_calories_per_elf);
+}
